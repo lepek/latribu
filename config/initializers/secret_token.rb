47e460523930fb25137678e9ffedccb5e1b47be4
Martin Bianculli <mbianculli@gmail.com>
2013-10-25 05:18:51 -0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nahual::Application.config.secret_key_base = '12ea0aa921d67dfd98f3bbd92bd4dd0e58976654428112c8dbd843492f20c1a7c90dc5e62a6a794698f5f036836f9fbe1404ae5674930646bc4f272859244413'
