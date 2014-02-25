22fb6f5400f3996da5471e1d89eb1bc665283677
Martin Bianculli <mbianculli@gmail.com>
2014-02-25 02:19:12 -0300

    Agregando la configuracion del host para el envio de mails y ocultando los anotados para las clases cerradas en la vista de administrador

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Nahual::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'nahual.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
