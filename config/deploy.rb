7ee88288255b67ac65ab3cb6e6e1a032c23f7224
Martin Bianculli <mbianculli@gmail.com>
2014-07-31 01:19:34 -0300

    MySQL como DB de produccion

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,7 @@ set :repo_url,  "https://github.com/lepek/latribu.git"
 set :scm, :git
 set :branch, 'master'
 set :pty, true
+
 # default_run_options[:pty] = true
 #ssh_options[:forward_agent] = true
 
@@ -21,51 +22,52 @@ namespace :deploy do
     desc "#{command} unicorn server"
     task command do
       on roles(:app), except: {no_release: true} do
-      execute "/etc/init.d/unicorn_#{fetch(:application)} #{command}"
+        execute "/etc/init.d/unicorn_#{fetch(:application)} #{command}"
+      end
     end
   end
-end
 
-task :setup_config do
-  on roles(:app) do
-    execute "mkdir -p #{shared_path}/config"
-    # execute File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
-    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{fetch(:application)}"
-    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{fetch(:application)}"
-  end
-end
-
-
-task :symlink_config do
-  on roles(:app) do
-    execute "ln -nfs #{release_path}/config/database.example.yml #{shared_path}/config/database.yml"
-    execute "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-  end
-end
-
-
-desc "Make sure local git is in sync with remote."
-task :check_revision do
-  on roles(:web) do
-    `git describe --tags --abbrev=0 > version`
-    unless `git rev-parse HEAD` == `git rev-parse origin/master`
-      puts "WARNING: HEAD is not the same as origin/master"
-      puts "execute `git push` to sync changes."
-      exit
+  task :setup_config do
+    on roles(:app) do
+      execute "mkdir -p #{shared_path}/config"
+      # execute File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+      sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{fetch(:application)}"
+      sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{fetch(:application)}"
     end
   end
-end
 
-task :write_version do
-  on roles(:app) do
-    upload! "./version", "#{release_path}/"
+
+  task :symlink_config do
+    on roles(:app) do
+      execute "ln -nfs #{release_path}/config/database.example.yml #{shared_path}/config/database.yml"
+      execute "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    end
   end
-end
 
-before "deploy:symlink_config", "deploy:setup_config"
-after 'deploy:updating', "deploy:symlink_config"
-after "deploy:symlink_config", 'deploy:write_version'
-before "deploy", "deploy:check_revision"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:web) do
+      `git describe --tags --abbrev=0 > version`
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "execute `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  task :write_version do
+    on roles(:app) do
+      upload! "./version", "#{release_path}/"
+    end
+  end
+
+  before "deploy:symlink_config", "deploy:setup_config"
+  after 'deploy:updating', "deploy:symlink_config"
+  after "deploy:symlink_config", 'deploy:write_version'
+  before "deploy", "deploy:check_revision"
+end
 
 #hooks
 after 'deploy', 'deploy:cleanup'
