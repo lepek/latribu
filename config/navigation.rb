240d495fa80e8446a9047a4ff56146dec4c8a3be
Martin Bianculli <mbianculli@gmail.com>
2014-07-31 15:22:46 -0300

    Agregando helpers para cargar imagenes

--- config/navigation.rb
+++ config/navigation.rb
@@ -6,11 +6,11 @@ SimpleNavigation::Configuration.run do |navigation|
 
     # I have to evalute it here because current_user may not exist, the x.item :if it's just to control the render
     if user_signed_in?
-      top.item :profile, '<img src="/assets/nav_icons/user-icon.png" />' + [current_user.first_name, current_user.last_name].compact.join(' ') , main_app.edit_user_registration_path
-      top.item :credit, '<img src="/assets/nav_icons/money-icon.png" /> ' + current_user.credit.to_s, main_app.edit_user_registration_path(current_user)
+      top.item :profile, '<img src="'+image_url("nav_icons/user-icon.png")+'" />' + [current_user.first_name, current_user.last_name].compact.join(' ') , main_app.edit_user_registration_path
+      top.item :credit, '<img src="'+image_url("nav_icons/money-icon.png")+'" /> ' + current_user.credit.to_s, main_app.edit_user_registration_path(current_user)
     end
 
-    top.item :sign_in, '<img src="/assets/nav_icons/keys-icon.png" /> Ingresar', main_app.new_user_session_path, :unless => Proc.new { user_signed_in? }
-    top.item :sign_out, '<img src="/assets/nav_icons/logout.png" /> Cerrar Sesión', main_app.destroy_user_session_path, :method => :delete, :if => Proc.new { user_signed_in? }
+    top.item :sign_in, '<img src="'+image_url("nav_icons/keys-icon.png")+'" /> Ingresar', main_app.new_user_session_path, :unless => Proc.new { user_signed_in? }
+    top.item :sign_out, '<img src="'+image_url("nav_icons/logout.png")+'" /> Cerrar Sesión', main_app.destroy_user_session_path, :method => :delete, :if => Proc.new { user_signed_in? }
   end
 end
