b142bc54fe2fa001dd0a0916d7ad045226907563
Martin Bianculli <mbianculli@gmail.com>
2014-08-12 13:40:20 -0300

    El admin puede editar los usuarios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,29 @@ class UsersController < ApplicationController
     redirect_to root_path(:anchor => 'users')
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    params = user_params
+    params.delete('password') && params.delete('password_confirmation') if params[:password].blank?
+    respond_to do |format|
+      if @user.update_attributes(params)
+        format.html { redirect_to root_url, success: 'Usuario actualizado.' }
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:id, :first_name, :last_name, :phone, :email, :password, :password_confirmation)
+  end
+
 end
