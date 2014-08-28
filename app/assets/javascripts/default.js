e30d0d499117d02655540c1fd889b71fc6549f82
Martin Bianculli <mbianculli@gmail.com>
2014-08-28 12:15:06 -0300

    Mas limpieza de assets

--- app/assets/javascripts/default.js
+++ app/assets/javascripts/default.js
@@ -1,8 +1,9 @@
 //= require turbolinks
 //= require jquery
-//= require jquery_ujs
 //= require 'jquery-cookie/jquery.cookie.js'
 //= require 'main.js'
 //= require select2
-//= require jquery-ui
+//= require jquery-ui/button
+//= require jquery-ui/tabs
+//= require jquery-ui/datepicker
 //= require jquery-ui/datepicker-es
