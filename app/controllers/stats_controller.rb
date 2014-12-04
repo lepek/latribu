4fd2fce81f5fe22bf9bc1c717e8c96899b9328ee
Martin Bianculli <mbianculli@gmail.com>
2014-12-04 18:59:15 -0300

    Cambio enlas estadisticas para mostrar el promedio

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -7,10 +7,14 @@ class StatsController < ApplicationController
 
   def month_inscriptions_chart
     @month = params[:id]
+    @days = Inscription.where(
+        :shift_date => Chronic.parse("#{@month} 00:00:00")..Chronic.parse("#{@month}").end_of_month
+    ).group("weekday(shift_date)").order("weekday(shift_date) ASC").pluck("count(distinct(DATE_FORMAT(shift_date, '%W%d')))")
+
     @inscriptions = Inscription.where(
         :shift_date => Chronic.parse("#{@month} 00:00:00")..Chronic.parse("#{@month}").end_of_month
-    ).group("DATE_FORMAT(shift_date, '%W%H%i')").order("count(id) DESC").pluck("shift_date, count(id)")
-    @inscriptions.map! { |stat| [I18n.l(stat.first, :format => '%A %H:%M hs.'), stat[1]] }
+    ).group("DATE_FORMAT(shift_date, '%W%H%i')").order("shift_date ASC").pluck("shift_date, count(id), weekday(shift_date)")
+    @inscriptions.map! { |stat| [I18n.l(stat.first, :format => '%A %H:%M hs.'), (stat[1].to_f/@days[stat[2]]).round(2) ] }
     render json: @inscriptions
   end
 end
