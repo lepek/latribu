3763dbeff129964275acfb0e87950dbd3fa321c7
Martin Bianculli <mbianculli@gmail.com>
2015-10-19 16:46:31 -0700

    Deploy config for temporary version

--- config/deploy/production2.rb
+++ config/deploy/production2.rb
@@ -1,11 +1,15 @@
-set :stage, :production
+set :stage, :production2
 server 'latriburosario.com', user: 'deployer', roles: %w{web app db}
 
 set :ssh_options, { forward_agent: false}
 
-set :nginx_server_name, "clases.latriburosario.com"
+set :nginx_server_name, "nuevo.latriburosario.com"
 #set :unicorn_user, "nginx"
 
 # dont try and infer something as important as environment from
 # stage name.
 set :rails_env, :production
+set :deploy_to, "/home/#{fetch(:user)}/apps/latriburosario_nuevo"
+set :branch, 'ajax-datatables'
+
+
