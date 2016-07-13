25f4fa3e6e09d05dbee6896935cc7f3aae9fa92d
Martin Bianculli <mbianculli@gmail.com>
2016-07-13 11:26:14 -0300

    Valid payments in bold

--- app/datatables/user_payment_datatable.rb
+++ app/datatables/user_payment_datatable.rb
@@ -17,12 +17,16 @@ class UserPaymentDatatable < AjaxDatatablesRails::Base
 
   def data
     records.map do |record|
+      open_bold_tag, close_bold_tag = '', ''
+      unless record.reset_date
+        open_bold_tag, close_bold_tag = '<strong>', '</strong>'
+      end
       [
-        I18n.l(record.created_at, :format => '%A, %e de %B del %Y %H:%M hs.'),
-        I18n.l(record.credit_end_date, :format => '%A, %e de %B del %Y'),
-        record.amount,
-        record.credit,
-        record.used_credit,
+        open_bold_tag + I18n.l(record.created_at, :format => '%A, %e de %B del %Y %H:%M hs.') + close_bold_tag,
+        open_bold_tag + I18n.l(record.credit_end_date, :format => '%A, %e de %B del %Y') + close_bold_tag,
+        open_bold_tag + record.amount.to_s + close_bold_tag,
+        open_bold_tag + record.credit.to_s + close_bold_tag,
+        open_bold_tag + record.used_credit.to_s + close_bold_tag,
         link_to('Editar', edit_payment_path(record), class: "btn btn-default") + ' ' + maybe_show_delete_button(record)
       ]
     end
